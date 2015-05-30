1d4bda6b1223e51329c02ca27f9dae9ce5ef6512
Élvio <ak4iwa@gmail.com>
2015-05-30 07:15:05 +0100

    Menu Exit
    exit button

---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitApplication : MonoBehaviour {
+
+	// Use this for initialization
+	void byebye () {
+		Debug.Log ("Been here");
+		Application.Quit();
+	}
+
+}
