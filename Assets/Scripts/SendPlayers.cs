f937e9f0704467a5df2e1da2b6eb1b3c8cea92f0
Élvio <ak4iwa@gmail.com>
2015-05-30 08:37:50 +0100

    main menu
    menu upgrade

---
+++ Assets/Scripts/SendPlayers.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendPlayers : MonoBehaviour {
+
+	public void LoadLevel3P() {
+		Application.LoadLevel("mainScene");
+	}
+
+	public void LoadLevel2P() {
+		Application.LoadLevel("mainScene");
+	}
+
+}
