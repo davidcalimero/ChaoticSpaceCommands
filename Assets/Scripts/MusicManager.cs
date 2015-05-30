4a8e8ec3afd013e6562af541c494d832238ec833
David Silva <david.d.silva@ist.utl.pt>
2015-05-30 18:57:22 +0100

    v1.1

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -9,5 +9,6 @@ public class MusicManager : MonoBehaviour {
 		var go = GameObject.Find("Game Music"); //Finds the game object called Game Music, if it goes by a different name, change this.
 		go.GetComponent<AudioSource>().clip = newMusic; //Replaces the old audio with the new one set in the inspector.
 		go.GetComponent<AudioSource>().Play(); //Plays the audio. 
+        go.GetComponent<AudioSource>().loop = true;
 	}
 }
