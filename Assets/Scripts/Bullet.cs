23f081e37289107b8577bd345382aefecf625d2e
David Silva <david.d.silva@ist.utl.pt>
2015-05-30 05:55:42 +0100

    fire componet and wall prefabs

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    public float timeToLive = 1.5f;
+    public float velocity = 200;
+
+    private SpriteRenderer sprite;
+    private TrailRenderer trail;
+    private float startTime;
+
+	void Start ()
+    {
+        startTime = Time.time;
+        sprite = GetComponent<SpriteRenderer>();
+        trail = GetComponent<TrailRenderer>();
+        
+        //Add Force
+        Vector2 direction = Quaternion.Euler(0, 0, transform.eulerAngles.z) * Vector2.up;
+        GetComponent<Rigidbody2D>().AddForce(direction * velocity);
+	}
+	
+	void FixedUpdate()
+    {
+        FadeOut();
+
+        if (sprite.color.a <= 0)
+            Destroy(gameObject);
+    }
+
+    void FadeOut()
+    {
+        float t = (Time.time - startTime) / timeToLive;
+        Color color = sprite.color;
+        color.a = Mathf.SmoothStep(1.0f, 0.0f, t);
+        sprite.color = color;
+        trail.material.color = color;
+    }
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+
+	public string fireButton = "b";
+    public float fallBackForce = 25;
+    public GameObject bullet;
+    private Transform shootPoint;
+    private Rigidbody2D physics;
+	
+	void Start ()
+    {
+        shootPoint = transform.FindChild("ShootPoint");
+        physics = GetComponent<Rigidbody2D>();
+    }
+
+	void Update ()
+    {
+        if (Input.GetKeyUp(fireButton))
+        {
+            fire();
+        }
+	}
+
+    void fire()
+    {
+        //Add back force
+        Vector2 direction = Quaternion.Euler(0, 0, transform.eulerAngles.z) * -Vector2.up;
+        physics.AddForce(direction * fallBackForce);
+
+        //Instantiate bullet
+        Instantiate(bullet, shootPoint.position, transform.rotation);
+    }
+}
